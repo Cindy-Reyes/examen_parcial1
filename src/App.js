1e2023130954a056215655176ec08b640d576923
ing.cindyreyes <ing.reyesarce@gmail.com>
2026-02-05 12:22:47 -0600

    Primer commit - Evaluacion Parcial 1

--- src/App.js
+++ src/App.js
@@ -6,16 +6,26 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={profile} className="App-logo" alt="logo" />
+        <h1>Evaluación parcial 1</h1>
         <p>
-          Este es mi Hola Mundo de React - Cindy Reyes
+          Alumno(a): Cindy Alejandra Reyes Arce
         </p>
         <a
           className="App-link"
-          href="http://www.utd.edu.mx"
+          href="https://www.linkedin.com/in/cindy-alejandra-reyes-arce-6b78383aa?utm_source=share_via&utm_content=profile&utm_medium=member_android"
           target="_blank"
           rel="noopener noreferrer"
         >
-          VISITAR EL SITIO WEB DE LA UTD
+          LINKED IN DE MI PROFILE
+        </a>
+        <br></br>
+        <a
+          className="App-link"
+          href="http://127.0.0.1:5500/reyes_parcial1/public/descargas/descargas.html"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          DOCUMENTACIÓN PARCIAL 1
         </a>
       </header>
     </div>
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Prueba_componente from './Prueba_componente';
+import Calculadora from './Calculadora';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
